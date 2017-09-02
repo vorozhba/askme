62651924ecb22230cd418141d20789a30404be67
vorozhba <s.vorozhba@gmail.com>
2017-09-03 00:16:59 +0300

    registration, sessions, edit profile, authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
   root 'users#index'
 
-  resources :users
-  resources :questions
+  # Ресурс пользователей (экшен destroy не поддерживается)
+  resources :users, except: [:destroy]
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'show' => 'users#show'
+  # Ресурс вопросов (кроме экшенов :show, :new, :index)
+  resources :questions, except: [:show, :new, :index]
+
+  # Ресурс сессий (только три экшена :new, :create, :destroy)
+  resources :sessions, only: [:new, :create, :destroy]
+
+  # Синонимы путей — в дополнение к созданным в ресурсах выше.
+  #
+  # Для любознательных: синонимы мы добавили, чтобы показать одну вещь и потом
+  # их удалим.
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
