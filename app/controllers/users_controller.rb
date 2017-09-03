cf2712a282c08f891eb30d5ae387904626d01970
vorozhba <s.vorozhba@gmail.com>
2017-09-03 13:53:16 +0300

    conters for questions, answeres & unanswered questions with pluralizations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -91,6 +91,10 @@ class UsersController < ApplicationController
     # создаем болванку вопроса, вызывая метод build у результата вызова метода
     # @user.questions.
     @new_question = @user.questions.build
+
+    @questions_count = @questions.count
+    @answers_count = @questions.where.not(answer: nil).count
+    @unanswered_count = @questions_count - @answers_count
   end
 
   private
